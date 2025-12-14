0349595e57c90826c48294513b88a20599882a61
sarah hesham <sarah.h.nashaat@gmail.com>
2025-12-14 19:30:54 +0200

    route handler error with absloute path

--- src/lib/constants/base-url.constant.ts
+++ src/lib/constants/base-url.constant.ts
@@ -1,5 +1,2 @@
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://exam.elevateegy.com/api/v1";
-
-// export const LOCAL_BASE_URL =
-//   process.env.NEXT_PUBLIC_LOCAL_API_URL || "http://localhost:3000";
--- src/lib/services/diploma.service.ts
+++ src/lib/services/diploma.service.ts
@@ -1,12 +1,9 @@
-import { LOCAL_BASE_URL } from "@/lib/constants/base-url.constant";
 import { DiplomasResponse } from "@/lib/types/diplomas";
 
 export default async function diplomaService(
   page: number,
   limit: number = 3
 ): Promise<DiplomasResponse> {
-  const response = await fetch(
-    `${LOCAL_BASE_URL}/api/diplomas?page=${page}&limit=${limit}`
-  );
+  const response = await fetch(`/api/diplomas?page=${page}&limit=${limit}`);
   return response.json();
 }
